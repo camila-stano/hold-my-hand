4c121333ed0b2f7c3ce45c2b35d6cbacaf23cdaf
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-03-01 16:08:18 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'shelters/index'
-  get 'shelters/show'
   devise_for :users
   root to: 'pages#home'
+  resources :shelters, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
