0aaba1c496bd1d9a3e20751a42a1191e960547ae
camila stano <camilastano.f@gmail.com>
2021-03-01 16:51:00 -0300

    Rotas de lawyers ajustadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'lawyers/index'
-  get 'lawyers/show'
   devise_for :users
   root to: 'pages#home'
   
   resources :restaurants, only: [:index, :show]
   resources :shelters, only: [:index, :show]
+  resources :lawyers, only: [:index, :def show
+    @ = .find()
+  end
+  ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
