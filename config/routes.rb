c8b8f950fdf4de8d50597b4f97ecc518faa30329
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-01 16:51:07 -0300

    rota para maps criada, botões do home alterados

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   
   resources :restaurants, only: [:index, :show]
   resources :shelters, only: [:index, :show]
+
+  get '/maps', to: 'pages#maps', as: :maps
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
