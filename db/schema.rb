3445ad6432ced421f5f908ffeaf4bdc4a0124c48
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-02 16:00:19 -0300

    merge master

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_03_02_173839) do
-=======
 ActiveRecord::Schema.define(version: 2021_03_02_174028) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "comunications", force: :cascade do |t|
-    t.string "expression"
-=======
   create_table "communications", force: :cascade do |t|
     t.string "phrase"
->>>>>>> master
     t.string "translation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -142,10 +112,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_174028) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-=======
   add_foreign_key "reviews", "lawyers"
   add_foreign_key "reviews", "users"
->>>>>>> master
 end
