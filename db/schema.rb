1b2bae3188d226976215ad4003be02ce9220eebf
camila stano <camilastano.f@gmail.com>
2021-03-02 12:43:50 -0300

    HTML básico das views INDEX e SHOW de Communications

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_145009) do
+ActiveRecord::Schema.define(version: 2021_03_02_150654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "communications", force: :cascade do |t|
-    t.string "expression"
+    t.string "phrase"
     t.string "translation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
