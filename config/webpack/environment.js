7abe2b795c21be249ef74a7572aa9110dc26b55f
camila stano <camilastano.f@gmail.com>
2021-03-03 13:25:59 -0300

    Modal plugin funcionando

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,8 +6,10 @@ environment.loaders.delete('nodeModules');
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default']
   })
 );
