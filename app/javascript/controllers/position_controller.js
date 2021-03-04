72d13f23e4c333e9c7d6503d1e8cb21235853df7
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-04 20:10:00 -0300

    load

--- app/javascript/controllers/position_controller.js
+++ app/javascript/controllers/position_controller.js
@@ -7,20 +7,23 @@ export default class extends Controller {
     }
   
   getPosition() {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      const position = pos.coords;
-      const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
-      
-      console.log(position.latitude)
-      fetch('/update_position', {
-        method: "POST",
-        headers: { 'X-CSRF-Token': csrfToken }, 
-        body: JSON.stringify({
-          lat: position.latitude,
-          lng: position.longitude
+    // load() {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const position = pos.coords;
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+        
+        
+        fetch('/update_position', {
+          method: "POST",
+          headers: { 'X-CSRF-Token': csrfToken }, 
+          body: JSON.stringify({
+            lat: position.latitude,
+            lng: position.longitude
+          })
         })
-      },
-      () => {console.log('deu ruim')});
-    })
+      }, () => {
+        console.log("Position unavailable.")
+      })
+    // }
   }
 }
