1645c24575015e12b9c186af4fd50ba2adf7594f
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-06 14:06:18 -0300

    index de lawyers e seed refatorado

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -2,6 +2,6 @@ class Lawyer < ApplicationRecord
   has_many :reviews
   has_one_attached :photo
 
-  validates :description, length: { maximum: 250,
+  validates :description, length: { maximum: 350,
   too_long: "%{count} characters is the maximum allowed" }
 end
