41460f634be064aa740936bf338b94e33c5e64f6
camila stano <camilastano.f@gmail.com>
2021-03-06 14:11:34 -0300

    Descrição dos documentos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_212009) do
+ActiveRecord::Schema.define(version: 2021_03_06_154129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_212009) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "description"
   end
 
   create_table "lawyers", force: :cascade do |t|
