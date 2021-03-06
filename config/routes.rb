c0fd25c6e880e762f66e72ae953cd307abf2380c
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-05 22:02:23 -0300

    rota de edit-user ajustada

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :users, skip: :omniauth_callbacks
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 
   scope "(:locale)", locale: /en|es|pt-BR/ do
