76c7fffcc2d61c3c0fd248ff85195902e699f50b
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-10 10:36:36 -0300

    rota do segundo formulário ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :origin, :photo, :nickname,
-                                                       :age, :language, :current_address, :gender, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :origin, :photo, :nickname])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:age, :language, :current_address, 
-                                                              :gender, :photo, :nickname])
+                                                              :gender, :photo, :nickname, :origin])
   end
 
   def default_url_options
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,6 +23,6 @@ class ProfilesController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:age, :language, :current_address, :gender)
+    params.require(:user).permit(:age, :language, :current_address, :gender, :photo, :nickname, :origin)
   end
 end
