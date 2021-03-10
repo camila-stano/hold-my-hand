6047d0ed3e5a6823ae7338dfc11f8a849ed11c86
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-09 22:29:49 -0300

    adicionei colunas aos usuários

--- db/migrate/20210310010220_add_about_to_users.rb
+++ db/migrate/20210310010220_add_about_to_users.rb
@@ -1,9 +1,9 @@
 class AddAboutToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :about, :text
-    add_column :users, :document, :boolean
-    add_column :users, :studying_pt, :boolean
-    add_column :users, :got_job, :boolean
+    add_column :users, :document, :boolean, default: :false
+    add_column :users, :studying_pt, :boolean, default: :false
+    add_column :users, :got_job, :boolean, default: :false
     add_column :users, :arrived, :date
   end
 end
