a64f9d6d77bdde12042cae9c12b7b83a63684d63
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-10 09:52:32 -0300

    adc params ao user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :origin, :photo, :nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :origin, :photo, :nickname,
+                                                       :age, :language, :current_address, :gender, :photo])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:age, :language, :current_address, 
                                                               :gender, :photo, :nickname])
