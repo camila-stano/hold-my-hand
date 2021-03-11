1153df083361290b86aebb07f5453705d620a52f
Mariana Marinho <marianaapmarinho@gmail.com>
2021-03-11 19:29:15 -0300

    seed melhorado

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,21 +24,21 @@ puts '-----------------------------'
 
 # latitude: -23.547794222490886, longitude: -46.63069957419347
 # file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1614706687/bomprato_f0ruvk.jpg')
-restaurant = Restaurant.create!(name: "Bom prato", address: "Rua 25 de Março, 166, Centro, São Paulo", phone: '(11) 3334-2977', price: 2, opening_hours: "Das 10:30h às 14:00h")
+restaurant = Restaurant.create!(name: "Bom prato", address: "Rua 25 de Março, 166, Centro, São Paulo", phone: '(11) 3334-2977', price: 1, opening_hours: "10:30am - 14:00pm", description: 'Low-cost Brazilian food to needy and unemployed people! Serves 1800 meals a day. Children up to six years of age are treated free of charge.')
 # restaurant.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "Restaurant Bom Prato created"
 puts '-----------------------------'
 
 # latitude: -23.540053644785395, longitude -46.61847401652173
 # file = URI.open('https://cdn4.ecycle.com.br/cache/images/eDicas/alimentos/50-600-bom-prato-525.jpg')
-restaurant = Restaurant.create!(name: "Popular", address: "Rua Barão de Ladário, 204, Brás, São Paulo", phone: "(11) 2292-4151", price: 1, opening_hours: "Das 11:00h às 13:30h")
+restaurant = Restaurant.create!(name: "Popular", address: "Rua Barão de Ladário, 204, Brás, São Paulo", phone: "(11) 2292-4151", price: 1, opening_hours: "11:00am - 13:30pm", description: "The Popular restaurant offer healthy and affordable ready-to-eat meals, generating new eating habits and practices, encouraging the use of regional foods.")
 puts "Restaurant Popular created"
 puts '-----------------------------'
 # restaurant.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 
 # latitude: -23.56729892520476, longitude: -46.69768310488062
 # file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1614708902/sesc_bwr69u.png')
-restaurant = Restaurant.create!(name: "Comedoria Sesc Pinheiros", address: "Rua Pais Leme, 195, Pinheiros, São Paulo", phone: '(11) 3095-9400', price: 10, opening_hours: "Das 11:30h às 14:30h")
+restaurant = Restaurant.create!(name: "Comedoria Sesc Pinheiros", address: "Rua Pais Leme, 195, Pinheiros, São Paulo", phone: '(11) 3095-9400', price: 10, opening_hours: "11:30am - 14:30pm", description: "Sesc's eating spaces offer meals, sandwiches, snacks, salads, sweets, hot and cold drinks - healthy, balanced, diverse and affordable.")
 puts "Restaurant Comedoria Sesc Pinheiros created"
 puts '-----------------------------'
 # restaurant.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
@@ -151,35 +151,35 @@ puts '-----------------------------'
 
 # latitude: -23.551303380484015, longitude: -46.63400312457595
 file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1615049363/WhatsApp_Image_2021-03-06_at_13.47.06_4_ud8mjz.jpg')
-lawyer = Lawyer.create!(name: "Pedro Miguel Meinhartt", address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Civil Litigation", description: "Routinely go to court on behalf of clients are litigators, and many specialize in areas such as personal injury, corporate, tax, etc. The general term “civil litigator” refers to an attorney who represents clients in civil (not criminal) court, either as plaintiff or defendant.")
+lawyer = Lawyer.create!(name: "Pedro Miguel Meinhartt", email: 'pedromiguel@hmh.com', address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Civil Litigation", description: "Routinely go to court on behalf of clients are litigators, and many specialize in areas such as personal injury, corporate, tax, etc. The general term “civil litigator” refers to an attorney who represents clients in civil (not criminal) court, either as plaintiff or defendant.")
 lawyer.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "lawyer #{lawyer.name} created"
 puts '-----------------------------'
 
 # latitude: -23.551303380484015, longitude: -46.63400312457595
 file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1615049368/WhatsApp_Image_2021-03-06_at_13.47.06_2_iymojc.jpg')
-lawyer = Lawyer.create!(name: "Erika Baracchini", address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Immigration", description: 'Immigration lawyers help immigrants, and employers seeking to hire them, with legal issues related to the status of immigrants and their families. Immigration lawyers help with visas, green cards, citizenship, and asylum and refugee status. Immigration laws change constantly, so it’s important to consult an experienced attorney in these situations.')
+lawyer = Lawyer.create!(name: "Erika Baracchini", email: 'erikabaracchini@hmh.com', address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Immigration", description: 'Immigration lawyers help immigrants, and employers seeking to hire them, with legal issues related to the status of immigrants and their families. Immigration lawyers help with visas, green cards, citizenship, and asylum and refugee status. Immigration laws change constantly, so it’s important to consult an experienced attorney in these situations.')
 lawyer.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "lawyer #{lawyer.name} created"
 puts '-----------------------------'
 
 # latitude: -23.551303380484015, longitude: -46.63400312457595
 file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1615049373/WhatsApp_Image_2021-03-06_at_13.47.06_nxe1kl.jpg')
-lawyer = Lawyer.create!(name: "André Toledo", address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Immigration", description: 'Immigration lawyers help immigrants, and employers seeking to hire them, with legal issues related to the status of immigrants and their families. Immigration lawyers help with visas, green cards, citizenship, and asylum and refugee status. Immigration laws change constantly, so it’s important to consult an experienced attorney in these situations.')
+lawyer = Lawyer.create!(name: "André Toledo", email: 'atoledo@hmh.com', address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Immigration", description: 'Immigration lawyers help immigrants, and employers seeking to hire them, with legal issues related to the status of immigrants and their families. Immigration lawyers help with visas, green cards, citizenship, and asylum and refugee status. Immigration laws change constantly, so it’s important to consult an experienced attorney in these situations.')
 lawyer.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "lawyer #{lawyer.name} created"
 puts '-----------------------------'
 
 # latitude: -23.551303380484015, longitude: -46.63400312457595
 file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1615049365/WhatsApp_Image_2021-03-06_at_13.47.06_3_n2etjd.jpg')
-lawyer = Lawyer.create!(name: "João Guimarães ", address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Criminal Law", description: 'Criminal lawyers are attorneys who defend individuals and organizations against government charges in federal and state courts. Criminal lawyers understand the rules around arrest, arraignment, bail, pleas, proof, and related issues. Some of the most common crimes requiring criminal lawyers include homicide, sexual battery and assault.')
+lawyer = Lawyer.create!(name: "João Guimarães ", email: 'joaoguimaraes@hmh.com', address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Criminal Law", description: 'Criminal lawyers are attorneys who defend individuals and organizations against government charges in federal and state courts. Criminal lawyers understand the rules around arrest, arraignment, bail, pleas, proof, and related issues. Some of the most common crimes requiring criminal lawyers include homicide, sexual battery and assault.')
 lawyer.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "lawyer #{lawyer.name} created"
 puts '-----------------------------'
 
 # latitude: -23.551303380484015, longitude: -46.63400312457595
 file = URI.open('https://res.cloudinary.com/dn6lyapiu/image/upload/v1615049370/WhatsApp_Image_2021-03-06_at_13.47.06_1_e3qcub.jpg')
-lawyer = Lawyer.create!(name: "Renata Bento", address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Employment Law", description: 'Both employees and employers seek out employment lawyers to deal with legal issues related to the workplace.  Some of the most common types of cases handled by employment lawyers are contracts, wrongful termination, workplace harassment, retaliation, and workplace discrimination.' )
+lawyer = Lawyer.create!(name: "Renata Bento", email: 'renata@hmh.com', address: "Praça da Sé, 385 - Sé, São Paulo", phone: '(11) 3291-3777', oab: "#{rand(100..999)}.#{rand(100..999)}", area: "Employment Law", description: 'Both employees and employers seek out employment lawyers to deal with legal issues related to the workplace.  Some of the most common types of cases handled by employment lawyers are contracts, wrongful termination, workplace harassment, retaliation, and workplace discrimination.' )
 lawyer.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 puts "lawyer #{lawyer.name} created"
 puts '-----------------------------'
