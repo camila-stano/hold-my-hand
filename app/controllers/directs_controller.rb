e84c3247fcc7acde0c1a4332290130d52805bba9
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-03-12 13:18:29 -0300

    done

--- app/controllers/directs_controller.rb
+++ app/controllers/directs_controller.rb
@@ -15,6 +15,8 @@ class DirectsController < ApplicationController
       @chatroom = Chatroom.create!(name: "chat-#{@user.id}-#{current_user.id}", direct: true)
       ChatMember.create(chatroom: @chatroom, user: current_user)
       ChatMember.create(chatroom: @chatroom, user: @user)
+    else
+      @chatroom = Chatroom.where( name: ["chat-#{@user.id}-#{current_user.id}","chat-#{current_user.id}-#{@user. id}"]).first
     end
     redirect_to direct_path(@chatroom)
   end
